a47b2275089cc895d8cffddebf187216f5676607
Emanoni <dayideren@QQ.COM>
2014-12-30 00:32:54 +0800

    数据库更新

--- Application/Conf/config.php
+++ Application/Conf/config.php
@@ -1,11 +1,15 @@
 <?php
 return array(
+
+//sagsvrdb.mysql.rds.aliyuncs.com：3306
+//luanwa:luanwa
+//use luanwa;
 'DB_TYPE'   => 'mysql', // 数据库类型
-'DB_HOST'   => SAE_MYSQL_HOST_M, // 服务器地址
-'DB_NAME'   => SAE_MYSQL_DB, // 数据库名
-'DB_USER'   => SAE_MYSQL_USER, // 用户名
-'DB_PWD'    => SAE_MYSQL_PASS, // 密码
-'DB_PORT'   => SAE_MYSQL_PORT, // 端口
+'DB_HOST'   => 'sagsvrdb.mysql.rds.aliyuncs.com', // 服务器地址
+'DB_NAME'   => 'luanwa', // 数据库名
+'DB_USER'   => 'luanwa', // 用户名
+'DB_PWD'    => 'luanwa', // 密码
+'DB_PORT'   => 3306, // 端口
 'DB_PREFIX' => '', // 数据库表前缀 
 'DB_CHARSET'=> 'utf8', // 字符集
 'DB_DEBUG'  =>  false, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
