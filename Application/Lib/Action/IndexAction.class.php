681884b1150e5dd774de0bad5b5b2a2227e58187
Emanoni <dayideren@QQ.COM>
2015-01-03 01:19:29 +0800

    用户模块接口

--- Application/Lib/Action/IndexAction.class.php
+++ Application/Lib/Action/IndexAction.class.php
@@ -1,44 +1,85 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
+	
 	public function index(){
-		$d = M("Magazineset")->select();
-		$this->assign("model", $d);
-		$this->assign("magazines", M("Magazine")->where(array('msid' => $d[0]['id']))->select());
-		$this->display();
+		$action = $_GET['action'];
+		if(!$action)
+			redirect('index.html');
+		echo $this->$action();
+		return;
 	}
 	
-	public function ms2a(){
-		$ms = $_GET['ms'];
-		$m = M("Magazine")->where(array('msid' => $ms))->order("time desc")->getField('id');
-		$id =  M("Article")->where(array('mid' => $m))->order("sort")->getField('id');
-		redirect("/read?id=" . $id);
-	}
-	
-	public function m2a(){
-		$m = $_GET['mid'];
-		$id =  M("Article")->where(array('mid' => $m))->order("sort")->getField('id');
-		redirect("/read?id=" . $id);
-	}
-	
-	private function checkKey($value){
-		return $value == "daluanwali";
-	}
-	
-	public function read(){
-		if ($_POST['code'] && $this->checkKey($_POST['code'])) {
-			setcookie("code", $_POST['code'], time() + 3600 * 24 * 14);
-			$this->show("<script>window.location.href=window.location.href;</script>");
-			return;
-		} elseif (!$this->checkKey($_COOKIE['code'])) {
-			$this->show("<center style='margin-top:200px;'><form method='post'>邀请码：<input name='code' /><input type='submit' /></form></center>");
-			return;
+	public function login() {
+		$username = $_GET['username'];
+		$password = $_GET['password'];
+		$dbUser = M("User")->where(array('username' => $username))->find();
+		if ($dbUser === null) {
+			return $this->fail('用户名不存在');
+		} elseif ($dbUser['password'] !== sha1($password)) {
+			return $this->fail('密码不正确');
+		} else {
+			session('user', $dbUser);
+			return $this->succ('登录成功');
 		}
-		$id = $_GET['id'];
-		$article = M("Article")->find($id);
-		$this->assign("articles", M("Article")->where(array('mid' => $article['mid']))->order("sort")->select());
-		$this->assign("user", M("User")->find($article['uid']));
-		$this->assign("model", $article);
-		$this->display();
+	}
+	
+	public function logout() {
+		session('user', null);
+		return $this->succ('登出成功');
+	}
+	
+	public function getuser() {
+		$dbUser = session('user');
+		if ($dbUser === null) {
+			return $this->fail('未登录');
+		}
+		return $this->succ($dbUser);
+	}
+	
+	public function register() {
+		$username = $_GET['username'];
+		$password = $_GET['password'];
+		if (!$username) {
+			return $this->fail('用户名不能为空');
+		}
+		if (!$password) {
+			return $this->fail('密码不能为空');
+		}
+		if (M("User")->where(array('username' => $username))->count() > 0) {
+			return $this->fail('用户名已存在');
+		} elseif (M("User")->add(array('username' => $username, 'password' => sha1($password)))) {
+			$this->login();
+			return $this->succ('注册成功');
+		} else {
+			return $this->fail('未知错误');
+		}
+	}
+	
+	public function updateuser() {
+		$update = array();
+		$update['nickname'] = $_GET['nickname'];
+		$update['gender'] = $_GET['gender'] == 'male';
+		$update['introduce'] = $_GET['introduce'];
+		$password = $_GET['password'];
+		
+		$dbUser = session('user');
+		if ($dbUser === null) {
+			return $this->fail('未登录');
+		} elseif ($dbUser['nickname'] && sha1($password) != $dbUser['password']) {
+			return $this->fail('已经填写过资料了');
+		} elseif (M('User')->where(array('id' => $dbUser['id']))->save($update)) {
+			session('user', M('User')->find($dbUser['id']));
+			return $this->succ('资料更新成功');
+		} else {
+			return $this->fail('未知错误');
+		}
+	}
+	
+	private function fail($msg) {
+		return json_encode(array('success' => false, 'message' => $msg));
+	}
+	
+	private function succ($msg) {
+		return json_encode(array('success' => true, 'message' => $msg));
 	}
 }
